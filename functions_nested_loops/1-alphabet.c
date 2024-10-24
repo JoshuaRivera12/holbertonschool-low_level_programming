f78db118d2ff0c799a1a2d653779c7c6283f4ff3
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-24 15:44:38 -0400

    betty fixes

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - Prints the alphabet in lowercase
  *
- *
- * Return: Always 0 (Success)
+ * Return: void
  */
-
 void print_alphabet(void)
 {
 	char ch;
@@ -17,3 +15,4 @@ void print_alphabet(void)
 	}
 	_putchar('\n');
 }
+
