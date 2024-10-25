c7bd8be425255ed5142eb4f5106fbaed9ff58b19
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-25 19:46:48 -0400

    adding number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - It adds 2 numbers together
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Added numbers
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -20,7 +20,7 @@ void jack_bauer(void);
 
 void times_table(void);
 
-int add(int, int);
+int add(int a, int b);
 
 void print_to_98(int n);
 
