bc62ea003afc15ef5fdbca6c59268f1cf547db95
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-25 17:02:25 -0400

    fixed betty

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,36 +7,33 @@
  */
 void times_table(void)
 {
-    int num, div, result;
+	int num, div, result;
 
-    for (num = 0; num < 10; num++)
-    {
-        for (div = 0; div < 10; div++)
-        {
-            result = num * div;
-
-            if (result > 9)
-            {
-                _putchar((result / 10) + '0');
-                _putchar((result % 10) + '0');
-            }
-            else if (div != 0)
-            {
-                _putchar(' ');
-                _putchar(result + '0');
-            }
-            else
-            {
-                _putchar(result + '0');
-            }
-
-            if (div != 9)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-        _putchar('\n');
-    }
+	for (num = 0; num < 10; num++)
+	{
+		for (div = 0; div < 10; div++)
+		{
+			result = num * div;
+			if (result > 9)
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (div != 0)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(result + '0');
+			}
+			if (div != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
-
