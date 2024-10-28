c09680bbdd17967e05f52c73d771b4dcabab6a0c
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-28 11:15:32 -0400

    counting numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* void print_numbers - Writes a function that prints numbers
+*
+* Return: Will always be 0
+*/
+
+void print_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
