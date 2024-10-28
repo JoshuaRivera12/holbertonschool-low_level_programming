ca047beaec58333280739ca484ce8501701e7680
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-28 14:27:09 -0400

    fix betty

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,13 @@
 void more_numbers(void)
 {
 	int n, num;
-	
+
 	for (n = 0; n < 10; n++)
 	{
 		for (num = 0; num <= 14; num++)
 	{
 		if (num > 9)
-		
-		_putchar((num /10) + '0');
+		_putchar((num / 10) + '0');
 		_putchar((num % 10) + '0');
 	}
 		_putchar('\n');
