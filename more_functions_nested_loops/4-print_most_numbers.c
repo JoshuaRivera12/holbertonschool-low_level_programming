985365782dbf90a778de684036e6d5671937e74f
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-28 11:34:54 -0400

    working test

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - writes a function that prints most numbers
+*
+* Return: Always 0
+*/
+
+void print_most_numbers(void)
+{
+	{
+	for (n = '0'; i <= '9'; n++)
+		if (n == '2' || i == '4')
+			continue;
+	_putchar(n)
+	}
+	_putchar('\n')
+}
