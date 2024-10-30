f42dc93602ddb78ded03a924fe41063303f2d117
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-30 13:55:24 -0400

    counting lines

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strlen - The function that prints the lenght of a string
+* @s: the legnth of a string
+*
+* Return: Always 0
+*/
+
+int _strlen(char *s)
+{
+	char l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
