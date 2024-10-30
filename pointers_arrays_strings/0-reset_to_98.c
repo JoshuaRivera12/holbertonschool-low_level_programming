f7cc6366376cc9e132bf6b4157243710a20a1b6b
JoshuaRivera12 <lobitrivera@gmail.com>
2024-10-30 09:32:53 -0400

    starts high end low

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* reset_to_98 - A function thats takes an int and updates the value to 98
+* @n: The value that is the reset
+*
+* Return: Always 0
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
