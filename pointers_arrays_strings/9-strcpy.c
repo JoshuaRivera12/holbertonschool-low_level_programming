6cde66b6048d7765c1bf10ea0bf49833b1252ef6
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-03 15:15:05 -0400

    fixed code

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,11 +9,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int a, b;
+    int a;
 
-	for (a = 0; src[a] != '\0'; a++)
-		;
-	for (b = 0; b < a; b++)
-		dest[b] = src[b];
-	return (dest);
+    for (a = 0; src[a] != '\0'; a++)
+        dest[a] = src[a];
+    dest[a] = '\0';  // Add null-terminator at the end of dest
+
+    return dest;
 }
+
