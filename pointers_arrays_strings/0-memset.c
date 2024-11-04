570e073ee91f00f313c74430dc917113416182eb
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-04 11:40:20 -0400

    betty fixes

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* memset - Function that fills the first part of bites of the memory area pointed by s
+* memset - Function that fills the first part of bites
 * @s: A value
 * @b: A value
 * @n: A value
@@ -13,9 +13,9 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	 unsigned int i = 0;
 
-	 for (; i < n; i++)
-	 {
-		 s[i] = b;
-	 }
-	 return (s);
+	for (i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
