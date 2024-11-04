d9bd53aba4f12d620a863c9757e2a140a2ab060d
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-04 11:16:28 -0400

    0x00 0x01x

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* memcpy - copys the code and paste it
+* @dest: the place the code is going to
+* @src: the code of the text
+* @n: N/A
+*
+* return: always 0
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for(; i < n; i++)
+	{
+		dest[i] = *src + i;
+	}
+	return (dest);
+}
