1715729b3d01f7adc7da34c24b76d862be2d33a1
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-06 13:39:47 -0400

    wanna play chess with me?

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints a chessboard
+ * @a: the rows of the chessboard
+ *
+ * Return: Void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, z;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (z = 0; z< 8; z++)
+		{
+			if (z ==7)
+			{
+				_putchar(a[i][z]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][z]);
+		}
+	}
+}
