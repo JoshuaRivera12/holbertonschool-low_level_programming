1d39b8df79e4f1e01a6656e98c2b4641ea116a99
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-08 10:32:27 -0400

    betty fixes

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -4,6 +4,8 @@
 * _pow_recursion - the function that will raise the power of x to the power y
 * @x: the value that will be raised to y
 * @y: the valuse x will be raised to
+*
+* Return: 0
 */
 
 int _pow_recursion(int x, int y)
