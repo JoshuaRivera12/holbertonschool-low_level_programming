149782fe7f295fc877cb946aeb375100804fa815
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-08 10:20:07 -0400

    x is now == y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _pow_recursion - the function that will raise the power of x to the power y
+* @x: the value that will be raised to y
+* @y: the valuse x will be raised to
+*/
+
+int _pow_recursion(int x, int y)
+{
+	int s = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+		return (x * _pow_recursion(s, y - 1));
+}
