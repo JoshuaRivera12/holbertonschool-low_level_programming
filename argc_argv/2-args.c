d8030ea3755152e4595cfac3695842da22d8a831
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-11 13:09:17 -0400

    betty fix

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
+* main - the starting point of the code
+* @argc: size of argv
+* @argv: the program being contaned
 *
-*
-*
-*
-*
+* Return: Always 0
 */
 
 int main(int argc, char *argv[])
