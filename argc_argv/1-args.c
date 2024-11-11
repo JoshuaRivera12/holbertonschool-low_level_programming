3baaf29cecdc8a5c1b86220adf5ae89d9dfd6765
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-11 10:30:39 -0400

    counting words

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - the starting point of the code
+* @argc: size of argc
+* @argv: the program being contaned
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
