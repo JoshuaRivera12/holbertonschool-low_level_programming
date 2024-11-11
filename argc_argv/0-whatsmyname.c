76c6098e3d2f48aa56b3767e2a6f150a34e82b72
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-11 08:40:19 -0400

    my new name is

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - the starting point of the code
+* @argc - size of argc
+* @argv: the program being contaned
+*
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
