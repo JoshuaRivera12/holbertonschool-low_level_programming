fe4d2f6e9a3d854224ac79fba52617f9895ac60d
JoshuaRivera12 <lobitrivera@gmail.com>
2024-11-15 13:12:44 -0400

    more files

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,18 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-
 /**
- * main - check the code
- *
- * Return: Always 0.
+ *_calloc - allocate memory for an array
+ *@nmemb: the size of the array
+ *@size: type of value
+ *Return: Return a pointer to the array
  */
-int main(void)
-{
-    char *concat;
 
-    concat = string_nconcat("Best ", "School !!!", 6);
-    printf("%s\n", concat);
-    free(concat);
-    return (0);
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, *nmall;
+	char *mall;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	if (size == sizeof(char))
+	{
+		mall = malloc(nmemb * size);
+		if (mall == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < nmemb; i++)
+		{
+			mall[i] = '0';
+		}
+		return (mall);
+	}
+	else
+	{
+		nmall = malloc(nmemb * size);
+		if (nmall == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < nmemb; i++)
+		{
+			nmall[i] = 0;
+		}
+		return (nmall);
+	}
 }
