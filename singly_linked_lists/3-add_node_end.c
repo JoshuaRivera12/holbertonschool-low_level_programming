518935c5dd850b8abf38aec673806367d10a29b0
JoshuaRivera12 <lobitrivera@gmail.com>
2024-12-08 20:47:59 -0400

    fixed

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,42 +1,33 @@
 #include "lists.h"
-#include "2-add_node.c"
-/**
- * add_node_end - add node on end of list
- * @head: singly list
- * @str: string
- * Return: modules found
- */
+#include <stdlib.h>
+#include <string.h>
 
+/**
+ * add_node_end - add a node in the end of a link list
+ * @head: the first node of the link list
+ * @str: the string in that node
+ * Return: Return the a pointer to the node
+*/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *pointerchain = NULL;
-	list_t *h = *head;
+	list_t *last = *head;
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
-	if (!h)
-	{
-		add_node(head, str);
-		return (*head);
-
-	}
-	while (h->next)
-	{
-		h = h->next;
-	}
-	pointerchain = malloc(sizeof(list_t));
-	if (!pointerchain)
-	{
-		free(pointerchain);
+	if (new_node == NULL)
 		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
 	}
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new_node;
 
-	pointerchain->str = strdup(str);
-	pointerchain->len = strlen(str);
-	pointerchain->next = NULL;
-
-	if (!head)
-		*head = pointerchain;
-	else
-		h->next = pointerchain;
-
-	return (pointerchain);
+	return (new_node);
 }
